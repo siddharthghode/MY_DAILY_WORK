1d6b796fc5a6d6fab9eb55355af7a47bcbefe170
siddharthghode <siddharthghode59911@gmail.com>
2025-12-20 00:40:23 +0530

    created structure for react projec

--- frond_end/react/day1/src/App.jsx
+++ frond_end/react/day1/src/App.jsx
@@ -1,5 +1,13 @@
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
