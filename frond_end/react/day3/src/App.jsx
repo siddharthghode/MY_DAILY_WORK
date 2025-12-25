61fb8f12992864b2302c8fb8782eebf4923133aa
siddharthghode <siddharthghode59911@gmail.com>
2025-12-25 18:04:16 +0530

    routing used

---
+++ frond_end/react/day3/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
