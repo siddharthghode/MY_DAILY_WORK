7103a3c16bbca83aeb8b23e6a4cb55327965211d
siddharthghode <siddharthghode59911@gmail.com>
2026-01-26 21:31:21 +0530

    inc/decr use state fuction

---
+++ frond_end/react/day11/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from "./pages/Home";
+import Counter from "./pages/Counter";
+
+function App(){
+return (
+<>
+<Home/>
+<Counter/>
+	</>
+);
+}
+export default App
