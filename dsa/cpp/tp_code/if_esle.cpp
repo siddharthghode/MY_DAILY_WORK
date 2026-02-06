0e60664f9441b5565932f273490ccb32f163290a
siddharthghode <siddharthghode59911@gmail.com>
2026-02-06 22:58:29 +0530

    revise:switch_if_else topics

---
+++ dsa/cpp/tp_code/if_esle.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a==1)
+    {
+        cout<<"Its one "<<endl;
+    }
+    else if(a=2)
+    {
+       cout<<"Its two"<<endl; 
+    }
+    else{
+        cout<<"The number is not one or two "<<endl;
+    }
+}
---
+++ dsa/cpp/tuf/demo.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string x;
+    getline(cin,x);
+    // cin>>x;
+    cout<<x;
+    return 0;
+}
