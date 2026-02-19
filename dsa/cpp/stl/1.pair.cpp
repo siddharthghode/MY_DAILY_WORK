4103c53b4c555c58ef38ad8c99b11f94bf165554
siddharthghode <siddharthghode59911@gmail.com>
2026-02-19 21:57:01 +0530

    stl.vectors

--- dsa/cpp/stl/1.pair.cpp
+++ dsa/cpp/stl/1.pair.cpp
@@ -4,10 +4,24 @@ using namespace std;
 
 void printpair()
 {
+    
     pair<int,int> p={1,2};
     cout<<p.first<<" "<<p.second<<endl;
     pair<pair<int,int>,int> p1={{2,5},6};
-    cout<<p1.first.first<<" "<<p1.first.second<<" "<<p1.second;
+    cout<<p1.first.first<<" "<<p1.first.second<<" "<<p1.second<<endl;
+    pair<pair<int,int>,pair<int,int>> p3={{1,4},{1,5}};
+    cout<<"1st pair 1st element="<<p3.first.first<<" "<<"2nd pair 2nd element="<<p3.second.second<<endl;
+    
+    pair<int,int> demo={2,5};
+    swap(demo,p);
+    cout<<p.first<<" "<<p.second<<endl;
+
+    //array of pair
+    pair<int,int> arr[]={{1,3},{3,4},{4,5}};
+    cout<<arr[1].second<<endl;
+
+    //bool
+    cout<<(p>demo);
 }
 int main ()
 {
---
+++ dsa/cpp/stl/2.vector.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ bool comparator(pair<int,int> p,pair<int,int> p1)
+   {
+      if (p.second<p1.second)return true;
+       return false;
+   }
+int main()
+{
+   vector<int> v={1,4,5,6,8,2};
+   v.push_back(0);
+   v.emplace_back(11);
+   cout<<v[1]<<endl;
+
+   vector<int>::iterator it;
+   for(it=v.begin();it!=v.end();it++)
+   {
+     cout<<*(it)<<" ";
+   }cout<<endl;   
+
+   sort(v.begin(),v.end());
+   cout<<"after sorting"<<endl; 
+   for(it=v.begin();it!=v.end();it++)
+   {
+     cout<<*(it)<<" ";
+   }
+   cout<<endl;
+   
+   cout<<endl<<"Vectors of pairs"<<endl;
+   vector<pair<int,int>> v1={{1,2},{1,1},{1,9},{1,4}};
+   cout<<v1[1].second<<endl;
+   v1.push_back({1,6});
+   v1.push_back({2,1});
+   v1.push_back({3,0});
+   v1.emplace_back(1,7);
+   vector<pair<int,int>>::iterator it1; //print all pairs
+   for(it1=v1.begin();it1!=v1.end();it1++)
+   {
+     cout<<(*it1).first<<" "<<(*it1).second<<endl; 
+   }
+
+
+   cout<<endl<<"Sorted vector of pairs"<<endl; //print all pairs in sorted order
+   sort(v1.begin(),v1.end(),greater<pair<int,int>>());
+   for(auto it=v1.begin();it!=v1.end();it++)
+   {
+      cout<<(*it).first<<" "<<(*it).second<<endl;
+   }
+
+
+   cout<<endl<<"Inverse sorted vector"<<endl; //print all pairs in inverse sorted order
+   sort(v1.begin(),v1.end(),greater<pair<int,int >>());
+   for(auto it=v1.begin();it!=v1.end();it++)
+   {
+      cout<<(*it).first<<" "<<(*it).second<<endl;
+   }
+
+   cout<<endl<<"Custom sorted vector on the bases of second element"<<endl; //print all pairs in custom sorted order
+   sort(v1.begin(),v1.end(),comparator);
+   
+   for(auto it=v1.begin();it!=v1.end();it++)
+      cout<<(*it).first<<" "<<(*it).second<<endl;
+   }
