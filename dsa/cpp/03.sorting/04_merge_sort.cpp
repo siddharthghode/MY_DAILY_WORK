d00e9d6707221c112cfb721ecc825acf747c0418
siddharthghode <siddharthghode59911@gmail.com>
2026-05-26 22:47:09 +0530

    NEW:finally mergesort

--- dsa/cpp/03.sorting/04_merge_sort.cpp
+++ dsa/cpp/03.sorting/04_merge_sort.cpp
@@ -1,6 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+
+void merge(vector<int>& v, int low, int mid, int high)
 {
-    
+    int left=low;
+    int right=mid+1;
+    vector<int> temp;
+    while(left<=mid && right<=high)
+    {
+        if(v[left]<=v[right])
+        {
+            temp.push_back(v[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(v[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+      temp.push_back(v[left]);
+      left++;
+    }
+    while(right<=high)
+    {
+      temp.push_back(v[right]);
+      right++;
+    }
+    for(int i=low;i<=high;i++)
+    {
+	    v[i]=temp[i-low];
+    }
+}
+
+void mergesort(vector<int>& v, int low, int high)
+{
+    if(low>=high)return;
+    int mid=(low+high)/2;
+    mergesort(v,low,mid);
+    mergesort(v, mid+1, high);
+    merge(v, low, mid, high);
+}
+int main ()
+{
+    vector<int> v={5,3,2,1,4};
+    mergesort(v,0,v.size()-1);
+    for(auto i:v)cout<<i<<" ";
 }
