320c97d3ed599a8b11f37aec63f75bbb3f119b0c
siddharthghode <siddharthghode59911@gmail.com>
2026-06-07 19:46:44 +0530

    new files java/collections

--- dsa/java/04.hashing/A03HashIntUsingMap.java
+++ dsa/java/04.hashing/A03HashIntUsingMap.java
@@ -1,12 +1,21 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class A03HashIntUsingMap {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 1, 9};
-        Map<Integer, Integer> p = new TreeMap<>();
-        for (int x : arr) p.merge(x, 1, Integer::sum);
-        for (Map.Entry<Integer, Integer> e : p.entrySet())
-            System.out.println(e.getKey() + "->" + e.getValue());
+       int[] arr={1,2,1,2,1,2,6,6,4,3,9,9,10};
+       int n=arr.length;
+       HashMap<Integer,Integer> hash= new HashMap<>();
+       for(int i=0;i<n;i++)
+       {
+        if(hash.containsKey(arr[i])) {
+                hash.put(arr[i], hash.get(arr[i]) + 1);
+            }
+            else {
+                hash.put(arr[i], 1);
+            }
+       }
+       System.out.println(hash);
     }
 }
---
+++ dsa/java/striver_sheet/01_array/Remove_dublicates.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Remove_dublicates{
+    static void remove_dub(ArrayList<Integer> list)
+    {
+        int i=0;
+        for(int j=1;j<=list.size()-1;j++)
+        {
+            if(list.get(i)!=list.get(j))
+            {
+                i++;
+                list.set(i,list.get(j));
+            }
+        }
+        int k=i+1;
+        while(k<=list.size()-1)
+        {
+           list.remove(k);
+           i++;
+        }
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the number of elements for array");
+        Scanner sc = new Scanner(System.in);
+
+        int n=sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            int value=sc.nextInt();
+            list.add(value);
+        }
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        sc.close();
+        remove_dub(list);
+        System.out.println(list);
+    }
+}
