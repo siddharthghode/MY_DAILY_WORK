b2c32e044b9c33ab5647d8e55928b4f51d8bf9a5
siddharthghode <siddharthghode59911@gmail.com>
2026-06-29 06:47:03 +0530

    NEW:dsa

--- dsa/java/03.sorting/A05QuickSort.java
+++ dsa/java/03.sorting/A05QuickSort.java
@@ -1,13 +1,17 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class A05QuickSort {
     static int partition(int[] arr, int low, int high) {
         int pivot = arr[high], i = low - 1;
         for (int j = low; j < high; j++)
             if (arr[j] <= pivot) {
-                int tmp = arr[++i]; arr[i] = arr[j]; arr[j] = tmp;
+                int tmp = arr[++i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
             }
-        int tmp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = tmp;
+        int tmp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = tmp;
         return i + 1;
     }
 
@@ -20,7 +24,7 @@ public class A05QuickSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {10, 7, 8, 9, 1, 5};
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
--- dsa/java/basics/array/A2dArray.java
+++ dsa/java/basics/array/A2dArray.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class A2dArray {
     public static void main(String[] args) {
@@ -7,10 +7,13 @@ public class A2dArray {
         int a = sc.nextInt(), b = sc.nextInt();
         int[][] c = new int[a][b];
         System.out.println("Enter elements for 2D array of " + a + "X" + b + ":");
-        for (int i = 0; i < a; i++) for (int j = 0; j < b; j++) c[i][j] = sc.nextInt();
+        for (int i = 0; i < a; i++)
+            for (int j = 0; j < b; j++)
+                c[i][j] = sc.nextInt();
         System.out.println("Entered elements are:");
         for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) System.out.print(c[i][j] + " ");
+            for (int j = 0; j < b; j++)
+                System.out.print(c[i][j] + " ");
             System.out.println();
         }
     }
